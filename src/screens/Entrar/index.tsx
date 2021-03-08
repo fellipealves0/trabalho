8c8d7dfb11ff406096a363ddd03d9315a47368ab
fellipealves0 <hipealves65@gmail.com>
2021-03-08 16:46:48 -0300

    ok

--- src/screens/Entrar/index.tsx
+++ src/screens/Entrar/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { View, StyleSheet, Text, Button, TextInput } from 'react-native';
+import { View, StyleSheet, Text, Button, TextInput} from 'react-native';
 
 import EmailIcon from './../../assets/email.svg';
 
