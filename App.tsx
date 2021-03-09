a90c1bf8671f5b4d1e0d29415068cdc41d1a6695
fellipealves0 <hipealves65@gmail.com>
2021-03-09 19:08:45 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreens from './src/screens/Entrar/indexFuncao';
+import {StyleSheet, Text, View } from 'react-native';
+import LoginScreens from './src/screens/Entrar/index';
 
 
 export default function App() {
