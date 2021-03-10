5c381772f40d9c1f5dbbfbf35c13da4340f42052
fellipealves0 <hipealves65@gmail.com>
2021-03-10 12:00:58 -0300

    ok

--- src/screens/Entrar/indexfuncao.tsx
+++ src/screens/Entrar/indexfuncao.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import {ImageBackground, StyleSheet,Button, TextInput,Text,TouchableOpacity, View, LogBox, Image} from 'react-native';
+import { ActivityIndicator } from 'react-native';
+import {ImageBackground, StyleSheet,Button, TextInput,Text,TouchableOpacity, View, LogBox} from 'react-native';
+import { Image } from 'react-native-elements/dist/image/Image';
 import {useNavigation} from '@react-navigation/native';
 
-import { Avatar } from 'react-native-elements/dist/avatar/Avatar';
 
 
 export interface LoginScreenProps {
@@ -10,24 +11,25 @@ export interface LoginScreenProps {
 
 export default function LoginScreen (props: LoginScreenProps) {
     return (
-      <ImageBackground source={require('./../../assets/Logo.png')}
-                                   style={styles.background}>
+      <ImageBackground source={require('./../../assets/Background.jpeg')}
+                                   style={{width:'100%',height:'100%'}}>
+    <View>
+    <Image source={require('./../../assets/logo1.png')}
+     style={{left:30, width:286, flex:1, padding:150,}}/>
+    </View>
     
-<View style={styles.container}>
-      
-     </View>
-     <View>
+
+     <View style={styles.container}>
          
      </View>
-    
+
+     
+
   </ImageBackground>
 );
 }
 const styles = StyleSheet.create({
-    background: {width:'100%',height:'100%'},
     container: {
-        flex:1,
-        padding:10,
         flexDirection:'column',
         justiftyContent:'center',
         alignItens: 'center',
