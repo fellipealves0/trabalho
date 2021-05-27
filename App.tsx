6a59d6cd4c9f85183f29c31bc7fdaa3609eee2fc
fellipealves0 <hipealves65@gmail.com>
2021-05-26 21:59:36 -0300

    ok

--- App.tsx
+++ App.tsx
@@ -2,7 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View } from 'react-native';
 import { MainNavigation } from './src/screens/navigations';
+import firebase from 'firebase';
+import {firebaseConfig} from './src/config/firebase';
 
+firebase.initializeApp(firebaseConfig)
 
 export default function App() {
   return (<MainNavigation/>);
---
+++ src/config/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDaEy3Qoduzf3UhjZfy-IM0aauG36f3hv0",
+    authDomain: "trabalho-dc6b7.firebaseapp.com",
+    projectId: "trabalho-dc6b7",
+    storageBucket: "trabalho-dc6b7.appspot.com",
+    messagingSenderId: "862042602053",
+    appId: "1:862042602053:web:05fe0bab6af74b2102812c",
+    measurementId: "G-62KBFBCV2Y"
+  };
