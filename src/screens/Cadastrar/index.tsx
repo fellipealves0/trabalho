1f120a29ae3d71646fa1dd049fd33fd12150849e
fellipealves0 <hipealves65@gmail.com>
2021-05-26 21:29:35 -0300

    ok

--- src/screens/Cadastrar/index.tsx
+++ src/screens/Cadastrar/index.tsx
@@ -1,4 +1,4 @@
-// @ts-nocheck
+
 import * as React from 'react';
 import { ActivityIndicator, Linking } from 'react-native';
 import {ImageBackground, StyleSheet, TextInput,Text,View, LogBox} from 'react-native';
@@ -8,6 +8,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {Formik} from 'formik';
 import * as Yup from 'yup'
 import { useNavigation } from '@react-navigation/core';
+import firebase from "firebase";
 
 
 export interface SignupScreenProps {}
@@ -17,24 +18,19 @@ export default function SignupScreen (props: SignupScreenProps) {
   const nav = useNavigation()
   const [erro , setErro] = React.useState ('')
  
-  const cadastrar = ({nome, email,senha}: any) =>{ 
-    console.log('nome',nome);
-    console.log('email',email);
-    console.log('senha',senha);
-
-    if (nome== '' && email == '' && senha == '')
-        nav.navigate('Menu')
-  else
-  console.log('Email já cadastrado');
+  const cadastrar = async ({dados}: any) =>{ 
+    console.log(dados);
+    firebase.auth().createUserWithEmailAndPassword(dados.email,dados.senha);
+    nav.navigate('Menu')
+    console.log('Email já cadastrado');
 
 }
 
 return (<ImageBackground source={require('./../../assets/fundo2.jpg')} style={{width:'100%',height:'100%'}}>
 
 <Formik
-  initialValues={{nome:'', email:'', senha:''}}
+  initialValues={{email:'', senha:''}}
   validationSchema={Yup.object().shape({
-    nome:Yup.string().required('Nome obrigatorio'),
     email:Yup.string().email ('Precisa ser um Email').required('Email obrigatorio'),
     senha:Yup.string().min (6,'Pelo menos 6 caracteres').required('Senha obrigatorio'),
   })}
@@ -47,8 +43,6 @@ return (<ImageBackground source={require('./../../assets/fundo2.jpg')} style={{w
    style={{flex:1, padding:130,margin:7}}/>
 
   <View style={styles.container}> 
-       <Input placeholder='Digite seu nome' leftIcon={<Icon name='user-plus'size={20} color='black'/>} onChangeText={handleChange('nome')}/>
-       <Text style={styles.erro}>{errors.nome}</Text>
 
        <Input placeholder='Digite seu email' leftIcon={<Icon name='user'size={20} color='black'/>} onChangeText={handleChange('email')}/>
        <Text style={styles.erro}>{errors.email}</Text>
