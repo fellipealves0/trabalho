fa11e7d514978d7439c861d8d898beb5179dcccc
fellipealves0 <hipealves65@gmail.com>
2021-06-03 18:58:00 -0300

    OK

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -3,8 +3,9 @@ import { View, Text } from 'react-native';
 import { Header } from 'react-native-elements';
 
 export interface ToolbarProps {
+    titulo:string;
 }
 
 export function Toolbar (props: ToolbarProps) {
-    return <Header/>;
+    return <Header centerComponent={{text:props.titulo}}/>;
 }
