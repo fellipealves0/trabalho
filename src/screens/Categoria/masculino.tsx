9d87585e5473f8ae7e028576c0f094e9d1fab64b
fellipealves0 <hipealves65@gmail.com>
2021-06-13 13:22:59 -0300

    ok

--- src/screens/Categoria/masculino.tsx
+++ src/screens/Categoria/masculino.tsx
@@ -8,6 +8,7 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import { useNavigation } from '@react-navigation/core';
 import { useNetInfo } from '@react-native-community/netinfo';
 import firebase from 'firebase';
+import { ProdutoScreen } from '../../components/produto';
 
 
 export interface MasculinoScreenProps {
@@ -16,9 +17,22 @@ export interface MasculinoScreenProps {
 export function MasculinoScreen (props: MasculinoScreenProps) {
   return (<ImageBackground source={require('./../../assets/fundo2.jpg')} style={{width:'100%',height:'100%'}}>
         
+ <Toolbar titulo="Masculino" menu={true}/>
 
-  <Toolbar titulo="Masculino" menu={true}/>
-  </ImageBackground>
+<ProdutoScreen/>
+
+</ImageBackground>
   
   );
   }
+
+  const styles = StyleSheet.create({
+    titulo:{color:'black',fontSize:20, fontWeight: "bold",textDecorationLine: 'none', marginBottom:20},
+    nomeproduto:{fontSize:18,textAlign:'center'},
+    precode:{fontSize:18,textAlign:'center', textDecorationLine: 'line-through' },
+    precopor:{fontSize:18,textAlign:'center'},
+    container2:{color:'black',fontSize:18,textDecorationLine: 'none', textAlign:'left', margin:30},
+    button:{margin:20},
+    image:{padding:150}
+  });
+  
